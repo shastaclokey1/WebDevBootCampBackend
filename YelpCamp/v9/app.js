8a06cdfa5c8189ad04e4cd533ac43866797b51af
shastaclokey1 <shasta.clokey@vmuv.io>
2019-05-16 07:24:52 -0700

    got yelp camp working on my local host started a mongo server installed all the node packages I needed with npm started the node server on port 3000 accessed the webpage through the browser

--- YelpCamp/v9/app.js
+++ YelpCamp/v9/app.js
@@ -41,6 +41,6 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("The YelpCamp Server Has Started :)")
+app.listen(process.env.PORT || 3000, function(){
+    console.log("The YelpCamp Server Has Started :)");
 });
